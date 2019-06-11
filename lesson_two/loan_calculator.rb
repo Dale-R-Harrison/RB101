da7f23f4d73cb44f76709d1501a2fbb8f6ee558c
Dale Harrison <HarrisonRDale@gmail.com>
2019-06-11 14:17:19 +0900

    refactoring loan calculator

--- lesson_two/loan_calculator.rb
+++ lesson_two/loan_calculator.rb
@@ -1,11 +1,10 @@
 
-require 'pry'
 require 'bigdecimal'
 require 'bigdecimal/util'
 
 def calculatate_monthly_payment(amount, interest, total_months)
   total_months -= total_months * 2
-  amount * (interest / (1 - (1 + interest)**(total_months)))
+  amount * (interest / (1 - (1 + interest)**total_months))
 end
 
 def monthly_interest(apr)
@@ -18,40 +17,41 @@ end
 
 def prompt(message)
   puts "=> #{message}"
-end 
+end
 
 def valid_input(input)
-  input.to_f >= 0 && input != ''
+  input.to_f > 0 && input != '' 
 end
+
+system("clear")
 
 loop do
   prompt "Welcome to the Monthly Loan Payment Calculator."
 
-  
   prompt "Let's get started. First, what is the amount of your loan?"
   amount = ''
-  
+
   loop do
     amount = gets.chomp
     if valid_input(amount)
       amount = amount.to_f.to_d
       break
     else
-      prompt "Sorry, amount of loan must be a positive number. Please try again."
+      prompt "Sorry, amount of loan must be a positive number. Please re-enter."
     end
   end
 
-  prompt "Thank you! Next, what is the APR of your loan?" 
+  prompt "Thank you! Next, what is the APR of your loan?"
   prompt "Enter as only a number. Example: If your APR is 5%, enter 5)"
   interest = ''
-  
+
   loop do
     interest = gets.chomp
     if valid_input(interest)
       interest = monthly_interest(interest.to_f.to_d)
       break
     else
-      prompt "You must have an APR of 0% or more. Please try again."
+      prompt "You must have an APR of more than 0%. Please try again."
     end
   end
 
@@ -64,20 +64,23 @@ loop do
       total_months = loan_duration(years.to_f.to_d)
       break
     else
-      "Most enter a positive number of years. Please try again."
+      prompt "Most enter a positive number of years. Please try again."
     end
   end
 
-
   prompt "Thank you! Calculating now!"
-  monthly_payment = calculatate_monthly_payment(amount, interest, total_months).to_f.round(2)
+  monthly_payment = calculatate_monthly_payment(amount, interest, total_months)
+  monthly_payment = monthly_payment.to_f.round(2)
 
-  prompt "Your total monthly payment will be #{monthly_payment}."
+  prompt "Your total payment payment each month will be $#{monthly_payment}."
 
   prompt "Would you like to calculate another loan? If so, enter'Y'."
   again = gets.chomp.downcase
 
-  unless again == 'y'
-    break
-  end
+  break unless again.downcase.start_with?('y')
+
+  system("clear")
+
 end
+
+ prompt "Thank you for using MOnthly Loan Payment Calculator. Goodbye!"
